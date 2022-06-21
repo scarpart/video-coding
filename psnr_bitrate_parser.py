15db2fe501d8013e5b7a4c1e01adcd6e0d51fca7
arthur <aenartzr@gmail.com>
2022-06-21 16:50:51 -0300

    feat(psnr_bitrate_parser): added reusability features
    Made it so psnr_bitrate_parser now creates and saves unique txt files for each sequence encoded, and checks if said txt file already exists in target dir. If it doesn't, the script proceeds to encode the sequence, but if it does then the script does nothing as the result would be the same, thus saving time. Json config file was also changed and new optional settings for both vvenc and VTM were added in case more options need to be passed onto the terminal.

--- psnr_bitrate_parser.py
+++ psnr_bitrate_parser.py
@@ -24,15 +24,30 @@ def parse(to_be_parsed):
 
 for v in configs['videos']:
     div_index = v.index("_")                                             # gets the _ index to separate the video name as follows: Video_WIDTHxHEIGHT --> Video / WIDTHxHEIGHT
-    video_res = v[div_index+1:-4]                                          # uses aforementioned index to get the resolution only
-    video_cfg_filepath = "/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/per-sequence/{}.cfg".format(v[:div_index]) # gets the video name only and appends it to string to get its cfg file path
+    video_res = v[div_index+1:-4]                                        # uses aforementioned index to get the resolution only
+    video_name = v[:div_index]                                          # uses aforementioned index to get the name only
+    video_cfg_filepath = f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/per-sequence/{video_name}.cfg"
 
     # lines below input a command into the terminal and extract information from its output by means of a .txt file 
     for qp in configs['qps']:
-        if configs['encoder'] == "VTM":
-            os.system("/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/bin/EncoderAppStatic -c /home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/encoder_randomaccess_vtm.cfg -c {} -f {} -q {} > output.txt".format(video_cfg_filepath, str(num_frames), str(qp)))
+        name_txt = f"{configs['encoder']}_{video_name}_{str(configs['num_frames'])}frames_{str(qp)}qsize"
+        txt_dir = os.listdir('./individual_outcomes/')
+        
+        # handling different encoder options
+        if configs['encoder'] == "VTM":            
+            # configures VTM and directs its output to a unique txt file
+            # if name_txt already exists in target dir, the following script does not execute as the result is the same
+            if name_txt not in txt_dir:
+                os.system(f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/bin/EncoderAppStatic \
+                            -c /home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/encoder_randomaccess_vtm.cfg \
+                                -c {video_cfg_filepath} -f {str(configs['num_frames'])} -q {str(qp)} {configs['settings_VTM']}> {name_txt}.txt")
+
         elif configs['encoder'] == "vvenc":
-            os.system('/home/arthurscarpatto/VC/BD-Rate/vvenc/bin/release-static/vvencapp -i /home/arthurscarpatto/VC/BD-Rate/Video-Samples/{} --output=bit.266 --frames {} --qp {} > output.txt'.format(v, str(configs['num_frames']), str(qp)))
+            # configures vvenc and directs its output to a unique txt file
+            if name_txt not in txt_dir:
+                os.system(f"/home/arthurscarpatto/VC/BD-Rate/vvenc/bin/release-static/vvencapp -i /home/arthurscarpatto/VC/BD-Rate/Video-Samples/{v} \
+                    --output=bit.266 --frames {str(configs['num_frames'])} --qp {str(qp)} {configs['settings_vvenc']}> /individual_outcomes/{name_txt}.txt")
+
         bit_rate, psnr = parse("output.txt")
 
         # appends the information gathered into a csv file with all the relevant parameters  
