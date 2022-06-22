570bceae8c957aaa11d7daeff87a97b25210d40a
arthur <aenartzr@gmail.com>
2022-06-22 15:01:59 -0300

    feat(psnr_bitrate_parser): small but important changes
    psnr_bitrate_parser.py now encodes using every encoder stated in the array at configs.json one at a time. Also changed the script to include the .bin output files.

--- psnr_bitrate_parser.py
+++ psnr_bitrate_parser.py
@@ -11,6 +11,7 @@ file_cfgs.close()
 def parse(to_be_parsed):
     with open("./{}".format(to_be_parsed), 'r') as txt:
         text = txt.read().split("\n")
+        print(text)
         for line in text:
             if "YUV-PSNR" in line.split():
                 data_line = text[text.index(line)+1].split()
@@ -18,44 +19,47 @@ def parse(to_be_parsed):
         txt.close()
         bitrate = data_line[2]
         psnr = data_line[6]
+        line_time = text[-2].split()
+        total_time = line_time[2]
 
-    return bitrate, psnr
+    return bitrate, psnr, total_time
 
 
-for v in configs['videos']:
-    div_index = v.index("_")                                             # gets the _ index to separate the video name as follows: Video_WIDTHxHEIGHT --> Video / WIDTHxHEIGHT
-    video_res = v[div_index+1:-4]                                        # uses aforementioned index to get the resolution only
-    video_name = v[:div_index]                                          # uses aforementioned index to get the name only
-    video_cfg_filepath = f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/per-sequence/{video_name}.cfg"
+for encoder in configs['encoder']: 
+    for v in configs['videos']:
+        div_index = v.index("_")                                             # gets the _ index to separate the video name as follows: Video_WIDTHxHEIGHT --> Video / WIDTHxHEIGHT
+        video_res = v[div_index+1:-4]                                        # uses aforementioned index to get the resolution only
+        video_name = v[:div_index]                                          # uses aforementioned index to get the name only
+        video_cfg_filepath = f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/per-sequence/{video_name}.cfg"
 
-    # lines below input a command into the terminal and extract information from its output by means of a .txt file 
-    for qp in configs['qps']:
-         # creating a unique name for the txt file based on the parameters
-        name_txt = f"{configs['encoder']}_{video_name}_{str(configs['num_frames'])}frames_{str(qp)}qsize"
-        txt_dir = os.listdir('./individual-outcomes/') 
-        
-        # handling different encoder options
-        if configs['encoder'] == "VTM":            
-            # configures VTM and directs its output to a unique txt file
-            # if name_txt already exists in target dir, the following script does not execute as the result is the same
-            if name_txt not in txt_dir:
-                os.system(f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/bin/EncoderAppStatic \
-                            -c /home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/encoder_randomaccess_vtm.cfg \
-                                -c {video_cfg_filepath} -f {str(configs['num_frames'])} -q {str(qp)} {configs['settings_VTM']}> ./individual-outcomes/{name_txt}.txt")
+        # lines below input a command into the terminal and extract information from its output by means of a .txt file 
+        for qp in configs['qps']:
+            # creating a unique name for the txt file based on the parameters
+            outcome = f"{encoder}_{video_name}_{str(configs['num_frames'])}frames_{str(qp)}qsize"
+            txt_dir = os.listdir('./text-outcomes/') 
+            
+            # handling different encoder options
+            if encoder == "VTM":            
+                # configures VTM and directs its output to a unique txt file
+                # if name_txt already exists in target dir, the following script does not execute as the result is the same
+                if outcome not in txt_dir:
+                    os.system(f"/home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/bin/EncoderAppStatic \
+                                -c /home/arthurscarpatto/VC/BD-Rate/VVCSoftware_VTM-VTM-17.0/cfg/encoder_randomaccess_vtm.cfg  -b ./binary-outcomes/{outcome}.bin \
+                                    -c {video_cfg_filepath} -f {str(configs['num_frames'])} -q {str(qp)} {configs['settings_VTM']}> ./text-outcomes/{outcome}.txt")
 
-        elif configs['encoder'] == "vvenc":
-            # configures vvenc and directs its output to a unique txt file
-            if name_txt not in txt_dir:
-                os.system(f"/home/arthurscarpatto/VC/BD-Rate/vvenc/bin/release-static/vvencapp -i /home/arthurscarpatto/VC/BD-Rate/Video-Samples/{v} \
-                    --output=bit.266 --frames {str(configs['num_frames'])} --qp {str(qp)} {configs['settings_vvenc']}> ./individual-outcomes/{name_txt}.txt")
+            elif encoder == "vvenc":
+                # configures vvenc and directs its output to a unique txt file
+                if outcome not in txt_dir:
+                    os.system(f"/home/arthurscarpatto/VC/BD-Rate/vvenc/bin/release-static/vvencapp -i /home/arthurscarpatto/VC/BD-Rate/Video-Samples/{v} \
+                        --output=./binary-outcomes/{outcome}.bin --frames {str(configs['num_frames'])} --qp {str(qp)} {configs['settings_vvenc']}> ./text-outcomes/{outcome}.txt")
 
-        # parses the output txt file to acquire relevant information 
-        bit_rate, psnr = parse(f"individual-outcomes/{name_txt}.txt")
+            # parses the output txt file to acquire relevant information 
+            bit_rate, psnr, total_time = parse(f"text-outcomes/{outcome}.txt")
 
-        # appends the information gathered into a csv file with all the relevant parameters  
-        with open("./results.csv", 'a') as csv_file:
-            writer_object = writer(csv_file)
+            # appends the information gathered into a csv file with all the relevant parameters  
+            with open("./results.csv", 'a') as csv_file:
+                writer_object = writer(csv_file)
 
-            # appends : name of encoder - video sample name - video resolution - number of frames - quantization parameter - bit rate - psnr - optional settings
-            writer_object.writerow([configs['encoder'], v, video_res, configs['num_frames'], qp, bit_rate, psnr, configs[f"settings_{configs['encoder']}"]])
-            csv_file.close()
+                # appends : name of encoder - video sample name - video resolution - number of frames - quantization parameter - bit rate - psnr - optional settings
+                writer_object.writerow([encoder, v, video_res, configs['num_frames'], qp, bit_rate, psnr, configs[f"settings_{encoder}"], total_time])
+                csv_file.close()
